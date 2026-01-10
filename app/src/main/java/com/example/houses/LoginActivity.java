3ae155aa534331f95d10b3099c72bb4528ad6a3e
matvtvtv <matveicharniauski@gmail.com>
2026-01-11 02:08:53 +0300

    login

--- app/src/main/java/com/example/houses/LoginActivity.java
+++ app/src/main/java/com/example/houses/LoginActivity.java
@@ -1,18 +1,21 @@
 package com.example.houses;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.houses.model.LoginRequest;
 import com.example.houses.model.RegisterRequest;
 import com.google.gson.Gson;
 
@@ -26,68 +29,47 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class RegistrationActivity extends AppCompatActivity {
-
-    EditText etLogin, etName, etPassword;
+public class LoginActivity extends AppCompatActivity {
+    EditText etLogin, etPassword;
     private Button btnRegister,btnEnterance;
-    private RadioGroup rgRole;
 
     private final OkHttpClient client = new OkHttpClient();
     private final Gson gson = new Gson();
 
-    private static final String BASE_URL ="https://t7lvb7zl-8080.euw.devtunnels.ms/api/user/register";
+    private static final String BASE_URL ="https://t7lvb7zl-8080.euw.devtunnels.ms/api/user/login";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
 
         etLogin = findViewById(R.id.etLogin);
-        etName = findViewById(R.id.etName);
         etPassword = findViewById(R.id.etPassword);
         btnRegister = findViewById(R.id.btnRegister);
         btnEnterance = findViewById(R.id.btnEnterance);
 
-        rgRole = findViewById(R.id.rgRole);
 
 
-        btnRegister.setOnClickListener(v -> register());
-        btnEnterance.setOnClickListener(v -> {
+        btnEnterance.setOnClickListener(v -> login());
+        btnRegister.setOnClickListener(v -> {
             startActivity(new Intent(
-                    RegistrationActivity.this,
-                    LoginActivity.class
+                    LoginActivity.this,
+                    RegistrationActivity.class
             ));
         });
-
     }
 
-    private void register() {
+
+    public void login(){
         String login = etLogin.getText().toString().trim();
-        String name = etName.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
-
-
-
-        if (login.isEmpty() || name.isEmpty() || password.isEmpty()) {
+        if (login.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();
             return;
         }
-
-        int selectedId = rgRole.getCheckedRadioButtonId();
-        if (selectedId == -1) {
-            Toast.makeText(this, "Choose role", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        String role;
-        if (selectedId == R.id.rbParent) {
-            role = "PARENT";
-        } else {
-            role = "CHILD";
-        }
-
-        RegisterRequest dto = new RegisterRequest(login, name, password, role);
+        LoginRequest dto = new LoginRequest(login,password);
 
         String json = gson.toJson(dto);
 
@@ -100,15 +82,14 @@ public class RegistrationActivity extends AppCompatActivity {
                 .url(BASE_URL)
                 .post(body)
                 .build();
-
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 runOnUiThread(() ->
-                        Toast.makeText(RegistrationActivity.this,
+                        Toast.makeText(LoginActivity.this,
                                 "Server error: " + e.getMessage(),
                                 Toast.LENGTH_LONG).show());
-                Log.v("RedistationActivity", e.getMessage());
+                Log.v("LoginActivity", e.getMessage());
             }
 
             @Override
@@ -118,19 +99,16 @@ public class RegistrationActivity extends AppCompatActivity {
                     SharedPreferences prefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
                     prefs.edit()
                             .putString("login", login)
-                            .putString("role", role)
                             .apply();
 
                     runOnUiThread(() -> {
-                        Toast.makeText(RegistrationActivity.this,
-                                "Registration successful " ,
-                                Toast.LENGTH_SHORT).show();
-                        Toast.makeText(RegistrationActivity.this,
-                                role,
+                        Toast.makeText(LoginActivity.this,
+                                "Entrance successful " ,
                                 Toast.LENGTH_SHORT).show();
 
+
                         startActivity(new Intent(
-                                RegistrationActivity.this,
+                                LoginActivity.this,
                                 MainActivity.class
                         ));
                         finish();
@@ -138,12 +116,18 @@ public class RegistrationActivity extends AppCompatActivity {
 
                 } else {
                     runOnUiThread(() ->
-                            Toast.makeText(RegistrationActivity.this,
-                                    "User already exists",
+                            Toast.makeText(LoginActivity.this,
+                                    "Invalid login or password",
                                     Toast.LENGTH_SHORT).show());
                 }
             }
         });
+
+
+
     }
+
+
+
 
 }
---
+++ app/src/main/java/com/example/houses/model/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.houses.model;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    public String login;
+    public String password;
+
+    public LoginRequest(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+}
